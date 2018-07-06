a43745e7f8cc31eb6512fa9db6cce5c5cf768adb
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-06 12:12:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,8 @@ class TicTacToe
     user_input.to_i - 1
   end
   
-  def move(board, index, char)
-    board[index] = char
+  def move(index, char)
+    @board[index] = char
   end
   
   def position_taken? (board, index)
