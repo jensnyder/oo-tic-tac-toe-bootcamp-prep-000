33a3b438bfe6ed08ebbea4bba7aa4913f1fc0dbf
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-06 12:17:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,9 +123,9 @@ class TicTacToe
     end
   end
   
-  def winner(board)
-    if won?(board)
-      return board[won?(board)[0]]
+  def winner
+    if won?
+      return @board[won?(@board)[0]]
     end
   end
   
